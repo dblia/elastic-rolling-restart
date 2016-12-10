5143dfc2e20fc42113eca5ba689c282384665a45
Dimitris Bliablias <dblia@skroutz.gr>
2016-12-10 18:35:43 +0200

    Modify Main's module logic
    Make a minor modification to the Main's module logic by creating a new
    function used to restart a single node.

--- Main.hs
+++ Main.hs
@@ -11,18 +11,15 @@ import OptsParser       (parseOpts, argParser)
 import Process
 import Utils            (curlPutString)
 
+import Data.List.Split  (splitOn)
 import Network.Curl
 import Text.Printf      (printf)
 
+-- | Helper function to restart a single node
+restart :: String -> String -> String -> IO ()
+restart master node service = do
 
-main :: IO ()
-main = withCurlDo $ do
-  -- | Parse command-line arguments.
-  (master, nodes) <- parseOpts >>= argParser
-  putStrLn $ "Master: " ++ master
-  putStr "Nodes: "
-  mapM_ (\str -> putStr $ " " ++ str) nodes
-  putStrLn "\n"
+  putStrLn $ printf ">>>>>> Restarting node '%s'\n" node
 
   -- | Verify green cluster status.
   putStrLn ">>> Verifying green cluster status"
@@ -35,7 +32,6 @@ main = withCurlDo $ do
   putStrLn $ out ++ "\n"
 
   -- | Request node shutdown for node.
-  let node = head nodes
   putStrLn $ ">>> Requesting node shutdown for " ++ node
   out <- Cli.nodeShutdown node
   putStrLn $ out ++ "\n"
@@ -47,10 +43,48 @@ main = withCurlDo $ do
 
   -- | Restart the node.
   putStrLn $ printf ">>> Restarting node '%s'" node
-  Process.runCmd "sudo systemctl start esctl@1.service" Nothing
+  let [hostname, _] = splitOn ":" node
+  let cmd = unwords [C.cmdSudo, C.cmdSystemctl, "start", service]
+  Process.runCmd cmd (Just hostname)
   putStr "\n"
 
   -- | Wait for node to respond after restart.
   putStrLn $ printf ">>> Waiting for node '%s' to re-join the cluster" node
   Cli.waitForNodeJoin node
   putStr "\n"
+
+  -- | Verify at least yellow cluster status.
+  putStrLn ">>> Verifying at least yellow cluster status"
+  body <- Cli.waitForStatus master ["yellow", "green"]
+  putStrLn body
+
+  -- | Re-enabling routing shard allocation.
+  putStrLn ">>> Re-enabling routing shard allocation"
+  out <- Cli.shardAllocToggle C.shardAllocEnable master
+  putStrLn $ out ++ "\n"
+
+  -- | Verify green cluster status.
+  putStrLn ">>> Verifying green cluster status"
+  body <- Cli.waitForStatus master ["green"]
+  putStrLn body
+
+  putStrLn $ printf ">>>>>> Node '%s' restarted successfully!\n" node
+
+main :: IO ()
+main = withCurlDo $ do
+  -- | Parse command-line arguments.
+  (master, nodes, service) <- parseOpts >>= argParser
+  putStrLn $ "Service: " ++ service
+  putStrLn $ "Master: " ++ master
+  putStr "Nodes: "
+  mapM_ (\str -> putStr $ " " ++ str) nodes
+  putStrLn "\n"
+
+  -- | Rolling restart the cluster nodes.
+  mapM_ (\node -> restart master node service) nodes
+
+  -- | Finally, restart the master node.
+  let node = master
+  let master = head nodes
+  putStrLn $ printf "New master is '%s', last node '%s'" master node
+  restart master node service
