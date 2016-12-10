61a234ac0ae85920648a9b081329e673ec9c6ee2
Dimitris Bliablias <dblia@skroutz.gr>
2016-12-10 18:35:39 +0200

    Create the curlPostString utility method
    Along with `curlPutString` method we introduce the `curlPostString`
    method to allow performing both `PUT` and `POST` requests.
    
    Since those methods share the same functionality, we introduce a new
    helper method named `createRequest`, that both methods call, using the
    respective request method.

--- Utils.hs
+++ Utils.hs
@@ -5,6 +5,8 @@
 module Utils
   ( curlPutString        -- :: URLString
                          -- -> [String] -> Maybe String -> IO (CurlCode, String)
+  , curlPostString       -- :: URLString
+                         -- -> [String] -> Maybe String -> IO (CurlCode, String)
   ) where
 
 import Constants       as C
@@ -16,14 +18,26 @@ import Network.Curl
 
 -- | Performs a PUT request, and returns the response code and body.
 curlPutString :: URLString -> [String] -> Maybe String -> IO (CurlCode, String)
-curlPutString url pfs ctype =
+curlPutString = createRequest "PUT"
+
+-- | Performs a POST request, and returns the response code and body.
+curlPostString :: URLString -> [String] -> Maybe String -> IO (CurlCode, String)
+curlPostString = createRequest "POST"
+
+-- | Performs a PUT or POST request and returns the response code and body.
+createRequest :: String                -- ^ request method
+              -> URLString             -- ^ URL string
+              -> [String]              -- ^ request post fields
+              -> Maybe String          -- ^ request content-type
+              -> IO (CurlCode, String) -- ^ (response code, response body)
+createRequest method url pfs ctype =
   let content_type = fromMaybe C.curlPostDefaultContentType ctype
   in initialize >>= \ h -> do
     ref <- newIORef []
     setopt h (CurlCookieJar "cookies")
     setopt h (CurlFailOnError True)
     setopt h (CurlURL url)
-    setopt h (CurlCustomRequest "PUT")
+    setopt h (CurlCustomRequest method)
     setopt h (CurlPostFields pfs)
     setopt h (CurlHttpHeaders ["Content-Type: " ++ content_type])
     setopt h (CurlWriteFunction (gatherOutput ref))
