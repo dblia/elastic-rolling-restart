c1c0d72064e2043c6dbba0e75639ee86c7beba66
Dimitris Bliablias <dblia@skroutz.gr>
2016-12-10 18:34:18 +0200

    Add the tool's Main module
    As the commit message implies, this module contains the tool's core
    logic. Currently it simply parses the given command-line arguments and
    prints them to stdout.

---
+++ Main.hs
@@ -0,0 +1,23 @@
+{-
+  Module core logic.
+-}
+
+module Main where
+
+import Client as Cli
+import Opts (parseOpts, argParser)
+
+import Network.Curl
+
+
+main :: IO ()
+main = withCurlDo $ do
+  (master, nodes) <- parseOpts >>= argParser
+  putStrLn $ "Master: " ++ master
+  putStr "Nodes: "
+  mapM_ (\str -> putStr $ " " ++ str) nodes
+  putStrLn ""
+  putStrLn ""
+  -- Verify green cluster status
+  body <- Cli.waitForStatus master ["green"]
+  putStr body
