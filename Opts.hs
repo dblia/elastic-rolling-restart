0fa943646b0316a0855b65a7e3ace39cd3712b27
Dimitris Bliablias <dblia@skroutz.gr>
2016-12-10 18:33:39 +0200

    Introduce an option parser module
    This patch, introduces an option parser module to parse command-line
    arguments properly, using the `optparse-applicative` Haskell package
    (see [0]).
    
    Currently the program expects the cluster master node, to use for
    coordinating the restart procedure, and a list of cluster nodes to be
    restarted. The list of nodes should be concateneted with commas. Also
    note that for both option the respective elasticsearch port should be
    included.
    
    An example of our option parser help message follows:
    
    ```
    Usage: Opts (-m|--master MASTER) (-n|--nodes NODES)
      ElasticSearch cluster rolling restart tool.
    
    Available options:
      -h,--help                Show this help text
      -m,--master MASTER       The cluster master node; used to coordinate the
                               rolling restart operation.
      -n,--nodes NODES         The cluster node hostnames to restart; nodes should
                               be concatenated with commas: host1:port[,host2:port].
    ```
    
    https://hackage.haskell.org/package/optparse-applicative

---
+++ Opts.hs
@@ -0,0 +1,54 @@
+{-
+  Options Parser module.
+-}
+
+module Opts
+  ( Opts(..)
+  , optParser
+  , parseOpts
+  , argParser
+  ) where
+
+import Data.List.Split     (splitOn)
+import Options.Applicative
+
+
+-- | Command line arguments.
+data Opts = Opts
+  { optsMaster :: String
+  , optsNodes  :: String
+  } deriving (Eq, Ord, Show)
+
+
+-- | Command line argument parser.
+optParser :: Parser Opts
+optParser = helper <*> _parser
+  where
+    _parser :: Parser Opts
+    _parser = Opts
+      <$> strOption
+          ( long "master"
+           <> short 'm'
+           <> metavar "MASTER"
+           <> help "The cluster master node; used to coordinate the rolling\
+                   \ restart operation.")
+      <*> strOption
+            ( long "nodes"
+           <> short 'n'
+           <> metavar "NODES"
+           <> help "The cluster node hostnames to restart; nodes should be\
+                   \ concatenated with commas: host1:port[,host2:port].")
+
+
+parseOpts :: IO Opts
+parseOpts = execParser $ info optParser $
+     fullDesc
+  <> progDesc "ElasticSearch cluster rolling restart tool."
+
+
+argParser :: Opts -> IO (String, [String])
+argParser opts =
+  case opts of
+    Opts { optsMaster = master
+         , optsNodes  = nodes
+         } -> return (master, splitOn "," nodes)
